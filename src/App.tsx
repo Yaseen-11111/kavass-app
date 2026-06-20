5befde501d2337ee81c66362f55e3154b8d3dd29
yaseen rashid <yaseenrashid001@outlook.com>
2026-06-21 00:47:55 +0100

    fixing file paths for images and html preview

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export default function App() {
     const [colorTheme, setTheme] = useDarkMode();
 
     return (
-        <Router>
+        <Router basename="/kavass-app/">
             <RouteHandler>
             <div className="min-h-screen flex flex-col bg-background transition-colors duration-300 font-sans pt-20">
                 <Navbar toggleTheme={setTheme} currentTheme={colorTheme} />
