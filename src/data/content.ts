63640faed6124f9e3ef0ca82eb1f7e6063ec58ad
yaseen rashid <yaseenrashid001@outlook.com>
2026-06-20 07:59:32 +0100

    change

--- src/data/content.ts
+++ src/data/content.ts
@@ -52,7 +52,7 @@ export const portfolioData: PortfolioItem[] = [
         client: "Luxe Polished Nails, Fleet",
         category: "Independent Nail Bar",
         imageUrl: "NailBar.png", // Ensure you add this image to your public folder [cite: 459]
-        previewUrl: "./assets/previews/NailBar.html"
+        previewUrl: "assets/previews/NailBar.html"
     },
     {
         id: "p2",
@@ -60,7 +60,7 @@ export const portfolioData: PortfolioItem[] = [
         client: "Razor & Blade, Aldershot",
         category: "Independent Barber Shop",
         imageUrl: "Barber.png", // Ensure you add this image to your public folder [cite: 459]
-        previewUrl: "./assets/previews/BarberSite.html"
+        previewUrl: "assets/previews/BarberSite.html"
     },
     {
         id: "p3",
@@ -68,14 +68,14 @@ export const portfolioData: PortfolioItem[] = [
         client: "Apex Builders, Camberley",
         category: "Construction Company",
         imageUrl: "./assets/projects/Builder.png", // Ensure you add this image to your public folder [cite: 459]
-        previewUrl: "./assets/previews/BuilderSite.html"
+        previewUrl: "assets/previews/BuilderSite.html"
     },
     {
         id: "p4",
         title: "Saffron Indian",
         client: "Saffron Indian, Farnborough",
         category: "Restaurant",
-        imageUrl: "./assets/projects/Indian.png", // Ensure you add this image to your public folder [cite: 459]
+        imageUrl: "assets/projects/Indian.png", // Ensure you add this image to your public folder [cite: 459]
         previewUrl: "./assets/previews/SaffronKitchen.html"
     },
     {
@@ -83,7 +83,7 @@ export const portfolioData: PortfolioItem[] = [
         title: "Vanguard Legal",
         client: "Vanguard Legal, Guildford",
         category: "Legal Firm",
-        imageUrl: "/kavass-app/src/assets/projects/Legal.png", // Ensure you add this image to your public folder [cite: 459]
+        imageUrl: "assets/projects/Legal.png", // Ensure you add this image to your public folder [cite: 459]
         previewUrl: "/kavass-app/src/assets/previews/LawFirm.html"
     },
     {
