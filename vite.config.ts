1b7b43869c6de96569278fe4318a9674be067796
yaseen rashid <yaseenrashid001@outlook.com>
2026-06-20 06:29:10 +0100

    Fix: Update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
-    base: '/kavass-app/',
+    base: '/home',
     plugins: [
         react(),
         tailwindcss(), // This handles everything, including autoprefixer
