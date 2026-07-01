76ac3d5d62009cfc6d40f890167c764cf8bfc56c
yaseen rashid <yaseenrashid001@outlook.com>
2026-07-01 04:59:51 +0100

    added dev mode again, accidently removed/broke dev file paths, build still functioning

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,9 @@ import { HashRouter as Router } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
-// Logic: If we are in production, use the repo name. Otherwise, use root.
-const isProd = import.meta.env.PROD;
-const basename = isProd ? '/kavass-app/' : '/';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Router basename={basename}>
+        <Router>
             <App />
         </Router>
     </React.StrictMode>,
